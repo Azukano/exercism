bd1b8f5a0ced0d4309b42d27cf2a053c484be5cb
Azukano <sciasbreathoffire@gmail.com>
2026-06-22 12:13:36 +0800

    solved kitchen unit conversion

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,18 +1,55 @@
 defmodule KitchenCalculator do
-  def get_volume(volume_pair) do
-    {kitchen_util, vol} = volume_pair   
-    vol 
+
+  @conversion_table { 
+    {:milliliter, 1},
+    {:cup, 240},
+    {:fluid_ounce, 30}, 
+    {:teaspoon, 5}, 
+    {:tablespoon, 15} 
+  }
+
+  def get_volume(volume_pair) do 
+    { unit, vol } = volume_pair 
+    vol    
   end
 
   def to_milliliter(volume_pair) do
-    # Please implement the to_milliliter/1 functions
+    { unit, vol } = volume_pair
+    cond do
+      unit == elem(@conversion_table, 0) |> elem(0)   
+        -> { :milliliter, vol * (elem(@conversion_table, 0) |> elem(1)) }
+      unit == elem(@conversion_table, 1) |> elem(0)   
+        -> { :milliliter, vol * (elem(@conversion_table, 1) |> elem(1)) }
+      unit == elem(@conversion_table, 2) |> elem(0)   
+        -> { :milliliter, vol * (elem(@conversion_table, 2) |> elem(1)) }
+      unit == elem(@conversion_table, 3) |> elem(0)   
+        -> { :milliliter, vol * (elem(@conversion_table, 3) |> elem(1)) }
+      unit == elem(@conversion_table, 4) |> elem(0)   
+        -> { :milliliter, vol * (elem(@conversion_table, 4) |> elem(1)) }
+      unit == elem(@conversion_table, 5) |> elem(0)   
+        -> { :milliliter, vol * (elem(@conversion_table, 5) |> elem(1)) }
+    end
   end
 
   def from_milliliter(volume_pair, unit) do
-    # Please implement the from_milliliter/2 functions
+    { _, vol } = volume_pair
+    cond do
+      unit == elem(@conversion_table, 0) |> elem(0)   
+        -> { unit, vol / (elem(@conversion_table, 0) |> elem(1)) }
+      unit == elem(@conversion_table, 1) |> elem(0)   
+        -> { unit, vol / (elem(@conversion_table, 1) |> elem(1)) }
+      unit == elem(@conversion_table, 2) |> elem(0)   
+        -> { unit, vol / (elem(@conversion_table, 2) |> elem(1)) }
+      unit == elem(@conversion_table, 3) |> elem(0)   
+        -> { unit, vol / (elem(@conversion_table, 3) |> elem(1)) }
+      unit == elem(@conversion_table, 4) |> elem(0)   
+        -> { unit, vol / (elem(@conversion_table, 4) |> elem(1)) }
+      unit == elem(@conversion_table, 5) |> elem(0)   
+        -> { unit, vol / (elem(@conversion_table, 5) |> elem(1)) }
+    end
   end
 
   def convert(volume_pair, unit) do
-    # Please implement the convert/2 function
+    { unit, get_volume(to_milliliter(volume_pair)) / get_volume(to_milliliter({unit, 1})) }
   end
 end
